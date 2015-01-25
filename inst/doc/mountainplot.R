fcce0455e34afe3a69f9306974d7fce8ab3d0678
Kevin Wright <kw.stat@gmail.com>
2015-01-25 00:00:00 +0000

    version 1.0

---
+++ inst/doc/mountainplot.R
@@ -0,0 +1,33 @@
+## ----setup, echo=FALSE, results="hide"--------------------------------------------------
+library("knitr")
+opts_chunk$set(fig.align="center", fig.width=7, fig.height=7)
+options(width=90)
+
+## ---------------------------------------------------------------------------------------
+library("mountainplot")
+data(singer, package = "lattice")
+singer <- within(singer, {
+section <- voice.part
+section <- gsub(" 1", "", section)
+section <- gsub(" 2", "", section)
+section <- factor(section)
+})
+# Change levels to logical ordering
+levels(singer$section) <- c("Bass","Tenor","Alto","Soprano")
+
+## ----mtn--------------------------------------------------------------------------------
+require(latticeExtra) # for ecdfplot
+ecdfplot(~height|section, data = singer, groups=voice.part, type='l',
+         layout=c(1,4),
+         main="Empirical CDF",
+         auto.key=list(columns=4), as.table=TRUE)
+
+## ---------------------------------------------------------------------------------------
+mountainplot(~height|section, data = singer, groups=voice.part, type='l',
+                      layout=c(1,4),
+             main="Folded Empirical CDF",
+             auto.key=list(columns=4), as.table=TRUE)
+
+## ----finish, echo=FALSE, results="asis"-------------------------------------------------
+toLatex(sessionInfo(), locale=FALSE)
+
